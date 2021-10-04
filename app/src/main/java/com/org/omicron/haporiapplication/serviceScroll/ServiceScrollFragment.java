c8883bda541e848c8f1da297b9dbe01cdf371bf7
Alex <alexmiller968@gmail.com>
2021-10-05 11:49:07 +1300

    Continuing work on MongoDB API

--- app/src/main/java/com/org/omicron/haporiapplication/serviceScroll/ServiceScrollFragment.java
+++ app/src/main/java/com/org/omicron/haporiapplication/serviceScroll/ServiceScrollFragment.java
@@ -1,6 +1,10 @@
 package com.org.omicron.haporiapplication.serviceScroll;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -8,15 +12,9 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
-
 import com.org.omicron.haporiapplication.R;
 import com.org.omicron.haporiapplication.databinding.FragmentServiceScrollBinding;
 import com.org.omicron.haporiapplication.restAPI.RetrofitClient;
-import com.org.omicron.haporiapplication.restAPI.models.Services;
 
 import java.util.ArrayList;
 import java.util.List;
